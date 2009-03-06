edb51fb483da0dbf2dfa8853265f81b65a4efd3a
Brad Beattie <bradbeattie@gmail.com>
2009-03-06 13:28:54 -0800

    Adding "View all issues" links, although the created_on one doesn't work as the field doesn't display by default. Hrm.

--- lib/target_version_graph_hook.rb
+++ lib/target_version_graph_hook.rb
@@ -2,7 +2,7 @@
 class TargetVersionGraphHook < Redmine::Hook::ViewListener
   def view_versions_show_bottom(context = { })
   	if !context[:version].fixed_issues.empty?
-  		output = "<fieldset id='filters'><legend>#{ l(:label_graphs_total_vs_closed_issues) }</legend>"
+  		output = "<fieldset id='target_version_graph'><legend>#{ l(:label_graphs_total_vs_closed_issues) }</legend>"
 		output << tag("embed", :width => "100%", :height => 300, :type => "image/svg+xml", :src => url_for(:controller => 'graphs', :action => 'target_version_graph', :id => context[:version]))
 		output << "</fieldset>"
 		return output
