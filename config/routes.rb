3a5f4389c7a6006384784d46bc497eae75acfda9
jaugustin <jeremie.augustin@pixel-cookers.com>
2012-05-21 17:02:17 +0200

    fix routes.rb, add route for my page graph and roadmap graph

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'projects/:project_id/issues/growth', :controller => 'graphs', :action => 'issue_growth'
   map.connect 'issues/growth', :controller => 'graphs', :action => 'issue_growth'
   map.connect ':project_id/issue_growth_graph', :controller => 'graphs', :action => 'issue_growth_graph'
+  map.connect 'graphs/recent-status-changes', :controller=>"graphs", :action=>"recent_status_changes_graph"
+  map.connect 'graphs/recent-assigned-to-changes', :controller=>"graphs", :action=>"recent_assigned_to_changes_graph"
+  map.connect 'graphs/target-version/:id', :controller=>"graphs", :action=>"target_version_graph" 
 end
