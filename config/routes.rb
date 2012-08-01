b7d04e7d197b6964ab8628ebfd5c2e6469c8083c
unknown <dlt@DLT-LAPTOP.deif.com>
2012-08-01 14:25:16 +0200

    updated for redmine 2.0 and added a graph for open/closed bugs in project

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect 'projects/:project_id/issues/old', :controller => 'graphs', :action => 'old_issues'
-  map.connect 'issues/old', :controller => 'graphs', :action => 'old_issues'
-  map.connect ':project_id/issue_age_graph', :controller => 'graphs', :action => 'issue_age_graph'
-  map.connect 'projects/:project_id/issues/growth', :controller => 'graphs', :action => 'issue_growth'
-  map.connect 'issues/growth', :controller => 'graphs', :action => 'issue_growth'
-  map.connect ':project_id/issue_growth_graph', :controller => 'graphs', :action => 'issue_growth_graph'
-  map.connect 'graphs/recent-status-changes', :controller=>"graphs", :action=>"recent_status_changes_graph"
-  map.connect 'graphs/recent-assigned-to-changes', :controller=>"graphs", :action=>"recent_assigned_to_changes_graph"
-  map.connect 'graphs/target-version/:id', :controller=>"graphs", :action=>"target_version_graph" 
+RedmineApp::Application.routes.draw do  
+  match 'issues/old', :to => 'graphs#old_issues', :via => 'get'
+  match 'issues/issue_age_graph', :to => 'graphs#issue_age_graph', :via => 'get'
+  match 'projects/:project_id/issues/old', :to => 'graphs#old_issues', :via => 'get'
+  match ':project_id/issue_age_graph', :to => 'graphs#issue_age_graph', :via => 'get'
+  match 'issues/growth', :to => 'graphs#issue_growth', :via => 'get'
+  match 'issues/issue_growth_graph', :to => 'graphs#issue_growth_graph', :via => 'get'
+  match 'projects/:project_id/issues/growth', :to => 'graphs#issue_growth', :via => 'get'
+  match ':project_id/issue_growth_graph', :to => 'graphs#issue_growth_graph', :via => 'get'
+  match 'issues/bug_growth', :to => 'graphs#bug_growth', :via => 'get'
+  match 'issues/bug_growth_graph', :to => 'graphs#bug_growth_graph', :via => 'get'
+  match 'projects/:project_id/issues/bug_growth', :to => 'graphs#bug_growth', :via => 'get'
+  match ':project_id/bug_growth_graph', :to => 'graphs#bug_growth_graph', :via => 'get'
+  match 'graphs/recent-status-changes', :to => 'graphs#recent_status_changes_graph', :via => 'get'
+  match 'graphs/recent-assigned-to-changes', :to => 'graphs#recent_assigned_to_changes_graph', :via => 'get'
+  match 'graphs/target-version/:id', :to => 'graphs#target_version_graph', :via => 'get'  
 end
--- routes.rb
+++
@@ -1,4 +0,0 @@
-map.connect 'projects/:project_id/issues/old', :controller => 'graphs', :action => 'old_issues'
-map.connect 'issues/old', :controller => 'graphs', :action => 'old_issues'
-map.connect 'projects/:project_id/issues/growth', :controller => 'graphs', :action => 'issue_growth'
-map.connect 'issues/growth', :controller => 'graphs', :action => 'issue_growth'
